7afc311831550badbd74cbdaec69f5a33aa2c91a
bowen-anth <bowen.anth@gmail.com>
2023-12-15 13:05:05 -0700

    added input field to main and adjusted margins

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,19 +4,24 @@ function Main() {
 
   return (
     <>
-        <main className="main-index">
-            <button>Wash Car: $10</button>
-            <button>Mow Lawn: $20</button>
-            <button>Pull Weeds: $30</button>
-        </main>
-        <div>
+        <main className="main-container">
+            <input className="main-task-input" type="text" placeholder="Enter task" />
+            <div className="main-button-container">
+                <button className="main-task-button">Wash Car: $10</button>
+                <button className="main-task-button">Mow Lawn: $20</button>
+                <button className="main-task-button">Pull Weeds: $30</button>
+            </div>
+        <div className="main-invoice-container">
             <span className="main-task-span">Task</span> <span className="main-total-span">Total</span>
+        </div>
+        <div className="main-task-container">
             <span className="main-task-span">Wash Car</span> <span className="main-total-span">$10</span>
         </div>
         <div className="main-notes-span">
             <span>We accept cash, credit card, or PayPal</span> <span className="main-total-amount-span">$600</span>
         </div>
-        <button><img src="./src/assets/mail.svg" alt="mail icon" width="10px" /> Send invoice</button>
+        </main>
+        <button className="main-send-button"><img src="./src/assets/mail.svg" alt="mail icon" width="10px" /> Send invoice</button>
     </>
   )
 }
